464f07c3bf2fca49ea3762204e64796916b74dbb
RKishmar <romakishmar85@gmail.com>
2019-10-29 09:35:51 +0300

    Update lab2_6_tb.sv
    fixed 3 errors of 4. Need to improve generator  to work with DUT.

--- LAB2/tb/lab2_6_tb.sv
+++ LAB2/tb/lab2_6_tb.sv
@@ -18,53 +18,44 @@
   integer                                                 test_num; 
   integer                                                 errors;  
   
-  
-class transaction;
-  logic [ DATA_WIDTH - 1 : 0 ] data_msg;
-endclass : transaction
+  typedef logic [ DATA_WIDTH - 1 : 0 ] t_transaction;
+  t_transaction trn;
 
 class generator;
   task transmit_tsk(input int unsigned n,
-                    input mailbox #( transaction ) mbx );
-    transaction trn;
-    trn = new();
+                    input mailbox #( t_transaction ) mbx );
+    t_transaction trn;
      
     repeat ( n ) begin
-      $display( " Starting RANDOMIZER " );
-      trn.data_msg = $urandom%{ 2** DATA_WIDTH - 1 };
+      $display( " Starting randomizer " );
+      trn = $urandom%{ 2 ** DATA_WIDTH - 1 };
       mbx.put( trn );
-      $display( " put to mailbox is done " );
+      $display( " put to mbx done " );
     end 
   
   endtask : transmit_tsk
 endclass : generator
 
 class receiver;
-    task receiver_tsk( input mailbox #( transaction ) mbx );
-      transaction trn;
-      forever begin
-        $display( " Starting RECEIVER " );
-        mbx.peek( trn );
-        #5;
-        $display( "Sequence under test: trn.data_msg=%0h", trn.data_msg );
-        mbx.get( trn );
-        
-        data_temp = trn.data_msg;
-        $display( " DATA under test: trn.data_msg=%0h", data_temp );
-        result_expect = 0;
+  task receiver_tsk( input mailbox #( t_transaction ) mbx );
+    t_transaction trn;
+    forever begin
+      mbx.get( trn );
+      data_temp = trn;
+      $display( " Sequence under check: data = %0h ", data_temp );
+      result_expect = 0;
       
-        while ( data_temp !== 'b0 ) begin
-          data_temp = data_temp & ( data_temp - 1 );
-          result_expect += 1;
-        end 
+      while ( data_temp !== 'b0 ) begin
+        data_temp = data_temp & ( data_temp - 1 );
+        result_expect += 1;
+      end 
         
-        test_tsk ( trn.data_msg, result_expect );
-        
-          if ( test_num == TEST_LENGTH )  
-            summary_tsk();
-  
-      end
-    endtask : receiver_tsk
+      test_tsk ( trn, result_expect );
+       
+        if ( test_num == TEST_LENGTH )  
+          summary_tsk();
+    end
+  endtask : receiver_tsk
 endclass : receiver
 
 //----------------------------------------------------------------------------  
@@ -75,9 +66,10 @@ task test_tsk ( input logic [ DATA_WIDTH - 1 : 0 ] data_msg,
     
     data = data_msg;
     
+    @(posedge clk);
     @( result_valid == 1 ); 
-    #10; 
      
+    
     if ( result_expect !== result ) begin
       display_error ();
       errors = errors + 1;
@@ -86,7 +78,7 @@ task test_tsk ( input logic [ DATA_WIDTH - 1 : 0 ] data_msg,
     test_num = test_num + 1;
     
     $display( " test number: %d", test_num );
-            
+  
   end 
 endtask : test_tsk   
 
@@ -94,13 +86,13 @@ endtask : test_tsk
 task automatic display_error ();
   begin
     $display( " " );
-    $display( "Error! test iteration number = %d...", test_num );
+    $display( "ERROR! Test number = %d...", test_num );
     $display( "data was tested = %b", data );
     $display( "result received = %b", result );
     $display( "result expected = %b", result_expect );
     $stop;
   end
-endtask
+endtask : display_error
 
 
 task summary_tsk ();
@@ -110,16 +102,15 @@ task summary_tsk ();
     $display( " " );
     $stop;    
   end  
-endtask
+endtask : summary_tsk
 
 //----------------------------------------------------------------------------  
 
 module lab2_6_tb;
 
-  generator              gen;
-  receiver               rcv;
-  mailbox #(transaction) mbx;
-  
+  generator                  gen;
+  receiver                   rcv;
+  mailbox #( t_transaction ) mbx;
   
 //----------------------------------------------------------------------------  
   
@@ -157,8 +148,8 @@ module lab2_6_tb;
     rcv = new();
 
     fork
-       gen.transmit_tsk ( TEST_LENGTH, mbx );
-       rcv.receiver_tsk ( mbx );
+      gen.transmit_tsk ( TEST_LENGTH, mbx );
+      rcv.receiver_tsk ( mbx );
     join
     
   end  

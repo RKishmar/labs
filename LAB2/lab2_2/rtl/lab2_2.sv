6c9845babc76320ef03ac519f9fc351aab6f7459
RKishmar <romakishmar85@gmail.com>
2019-11-15 10:15:24 +0300

    a few adjustments to lab2_2
    a few adjustments

--- LAB2/lab2_2/rtl/lab2_2.sv
+++ LAB2/lab2_2/rtl/lab2_2.sv
@@ -1,18 +1,15 @@
-module lab2_2 #(
-  parameter DATA_WIDTH = 16
-)
+module lab2_2 #( parameter DATA_WIDTH )
 ( input                                   clk_i,
   input                                   srst_i,
   input   [ DATA_WIDTH - 1 : 0 ]          data_i,
   input   [ $clog2 (DATA_WIDTH) - 1 : 0 ] data_mod_i,
   input                                   data_val_i,
-  output                                  ser_data_o,
-  output                                  ser_data_val_o,
-  output                                  busy_o  
+  output logic                            ser_data_o,
+  output logic                            ser_data_val_o,
+  output logic                            busy_o  
 );
 
-integer                                   bit_num;
-
+integer                               bit_num;
 logic [ DATA_WIDTH - 1 : 0 ]          data_r;
 logic [ $clog2 (DATA_WIDTH) - 1 : 0 ] data_mod_r;
 
@@ -22,18 +19,15 @@ state_type curr_state, next_state;
 always_ff @( posedge clk_i )
   if ( !srst_i ) 
     begin
-	   curr_state <= IDLE;
-	 end
-  else
-    begin
-	   curr_state <= next_state;
+      curr_state <= IDLE;
   end
- 
+    else
+      curr_state <= next_state;
  
 always_comb begin
   unique case ( curr_state )
     IDLE : 
-	   begin
+      begin
         if( data_val_i )
           next_state = READ;
         else
@@ -41,15 +35,15 @@ always_comb begin
       end
     	 
     READ : 
-	   begin
-        if( data_mod_i < 4 )
+      begin
+        if( data_mod_i < 3 )
           next_state = IDLE;
-		  else 
-		    next_state = SEND;
+        else 
+          next_state = SEND;
       end
     
-	 SEND : 
-	   begin
+    SEND : 
+      begin
         if( bit_num == DATA_WIDTH - 1 - data_mod_r )
           next_state = IDLE;
         else 
@@ -57,54 +51,52 @@ always_comb begin
       end
 		
     default : 
-	   begin
-		  next_state = IDLE;		  
-		end
+      begin
+        next_state = IDLE;		  
+      end
 
   endcase
 end
  
  
 always_ff @( posedge clk_i )
-begin 
-  if ( !srst_i ) 
-    begin
-      busy_o         =  0;
-		ser_data_val_o =  0;
-		ser_data_o     = '0;
-		bit_num        =  DATA_WIDTH;
+  begin 
+    if ( !srst_i ) 
+      begin
+        busy_o         = 0;
+        ser_data_val_o = 0;
+        ser_data_o     = 0;
+        bit_num        = DATA_WIDTH;
   end
     else 
-	   begin
+      begin
         if ( curr_state == IDLE )
-		    begin
-            busy_o         =  0;
-		      ser_data_val_o =  0;
-		      ser_data_o     = '0;	
-            bit_num        =  DATA_WIDTH;				
-		  end
+          begin
+            busy_o         = 0;
+            ser_data_val_o = 0;
+            ser_data_o     = 0;	
+            bit_num        = DATA_WIDTH;				
+        end
 		  
-		  else if ( curr_state == READ )
-			 begin
-            busy_o         =  1;
-		      ser_data_val_o =  0;
-		      ser_data_o     = '0;	
-	         data_r         =  data_i;			
-				data_mod_r     =  data_mod_i;
-				bit_num        =  DATA_WIDTH;
-			 end
+        else if ( curr_state == READ )
+          begin
+            busy_o         = 1;
+            ser_data_val_o = 0;
+            ser_data_o     = 0;	
+            data_r         = data_i;			
+            data_mod_r     = data_mod_i;
+            bit_num        = DATA_WIDTH;
+        end
 			 
         else if ( curr_state == SEND )
-			 begin
+          begin
             busy_o         =  1;
-		      ser_data_val_o =  1;
-		      ser_data_o     = '0;	
+            ser_data_val_o = 1;
+            ser_data_o     = 0;	
             bit_num = bit_num - 1;
             ser_data_o = data_r [ bit_num ];	
-          end 
-		 
-			 
-			
+        end 
+
     end
 end 
 

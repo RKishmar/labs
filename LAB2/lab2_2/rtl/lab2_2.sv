bb4034bddcf8f84a55fefa2e2c144add4b319e07
RKishmar <romakishmar85@gmail.com>
2019-11-18 09:54:16 +0300

    lab2_2 - rtl
    fixed all review problems in rtl

--- LAB2/lab2_2/rtl/lab2_2.sv
+++ LAB2/lab2_2/rtl/lab2_2.sv
@@ -1,4 +1,4 @@
-module lab2_2 #( parameter DATA_WIDTH )
+module lab2_2 #( parameter DATA_WIDTH = 16 )
 ( input                                   clk_i,
   input                                   srst_i,
   input   [ DATA_WIDTH - 1 : 0 ]          data_i,
@@ -9,6 +9,7 @@ module lab2_2 #( parameter DATA_WIDTH )
   output logic                            busy_o  
 );
 
+localparam                            MIN_VALID_LEN = 3;
 integer                               bit_num;
 logic [ DATA_WIDTH - 1 : 0 ]          data_r;
 logic [ $clog2 (DATA_WIDTH) - 1 : 0 ] data_mod_r;
@@ -25,18 +26,17 @@ always_ff @( posedge clk_i )
       curr_state <= next_state;
  
 always_comb begin
+  next_state = curr_state;
   unique case ( curr_state )
     IDLE : 
       begin
         if( data_val_i )
           next_state = READ;
-        else
-          next_state = IDLE;
       end
     	 
     READ : 
       begin
-        if( data_mod_i < 3 )
+        if( data_mod_i < MIN_VALID_LEN )
           next_state = IDLE;
         else 
           next_state = SEND;
@@ -46,8 +46,6 @@ always_comb begin
       begin
         if( bit_num == DATA_WIDTH - 1 - data_mod_r )
           next_state = IDLE;
-        else 
-          next_state = SEND;
       end
 		
     default : 
@@ -63,38 +61,38 @@ always_ff @( posedge clk_i )
   begin 
     if ( !srst_i ) 
       begin
-        busy_o         = 0;
-        ser_data_val_o = 0;
-        ser_data_o     = 0;
-        bit_num        = DATA_WIDTH;
+        busy_o         <= 0;
+        ser_data_val_o <= 0;
+        ser_data_o     <= 0;
+        bit_num        <= DATA_WIDTH;
   end
     else 
       begin
         if ( curr_state == IDLE )
           begin
-            busy_o         = 0;
-            ser_data_val_o = 0;
-            ser_data_o     = 0;	
-            bit_num        = DATA_WIDTH;				
+            busy_o         <= ( data_val_i == 1'b1 ) ? 1'b1 : 1'b0;
+            ser_data_val_o <= 0;
+            ser_data_o     <= 0;	
+            bit_num        <= DATA_WIDTH;				
         end
 		  
         else if ( curr_state == READ )
           begin
-            busy_o         = 1;
-            ser_data_val_o = 0;
-            ser_data_o     = 0;	
-            data_r         = data_i;			
-            data_mod_r     = data_mod_i;
-            bit_num        = DATA_WIDTH;
+            busy_o         <= 1;
+            ser_data_val_o <= 0;
+            ser_data_o     <= 0;	
+            data_r         <= data_i;			
+            data_mod_r     <= data_mod_i;
+            bit_num        <= DATA_WIDTH;
         end
 			 
         else if ( curr_state == SEND )
           begin
-            busy_o         =  1;
-            ser_data_val_o = 1;
-            ser_data_o     = 0;	
-            bit_num = bit_num - 1;
-            ser_data_o = data_r [ bit_num ];	
+            busy_o         <= 1;
+            ser_data_val_o <= 1;
+            ser_data_o     <= 0;	
+            bit_num        <= bit_num - 1;
+            ser_data_o     <= data_r [ bit_num - 1 ];	
         end 
 
     end

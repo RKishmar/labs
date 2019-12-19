acc3e2d6337811357413c8fc155fd101bf438219
RKishmar <romakishmar85@gmail.com>
2019-12-19 07:29:06 +0300

    Update lab2_2_tb.sv
    added event-check solution. finally works.

--- LAB2/lab2_2/tb/lab2_2_tb.sv
+++ LAB2/lab2_2/tb/lab2_2_tb.sv
@@ -1,8 +1,9 @@
 `timescale 1ns/1ns
   
 
-  localparam DATA_WIDTH  = 16;
-  localparam TEST_LENGTH = 50000;
+  localparam DATA_WIDTH     = 16;
+  localparam MIN_VALID_LEN  = 3;
+  localparam TEST_LENGTH    = 100000;
 
   logic                                   clk;
   logic                                   reset;
@@ -19,16 +20,19 @@
 
 //---------------------------------------------------------------------------- 
 
+event length_check_ev;
+
 class transaction;
   rand bit [ DATA_WIDTH - 1 : 0 ]         data_tr;
   rand bit [ $clog2( DATA_WIDTH )-1 : 0 ] tr_size;
-  
+
   function void randomizing ();
-  
-    process::self.srandom(123);
   
     data_tr = $urandom%{ 2 ** DATA_WIDTH - 1 };
     tr_size = $urandom%{ DATA_WIDTH - 1 };
+    
+    $display ("random_data - ", data_tr);
+    $display ("random_size - ", tr_size);
     
   endfunction
   
@@ -45,9 +49,8 @@ endclass
       $display ("TRANSMIT START");
       trn.randomizing();
       mbx.put( trn );
-      //$display ("MAIL STUFFED");
       wait ( busy == 0 );
-	  
+	  	  
       @( posedge clk ) ;	  
       data_to_input = trn.data_tr;
       data_size     = trn.tr_size;
@@ -55,6 +58,18 @@ endclass
 	  
       @( posedge clk ) ;
       data_val      = 0;
+  
+	    if ( trn.tr_size >= MIN_VALID_LEN )
+	      begin
+	        -> length_check_ev;
+      end else
+        begin 
+	        mbx.get( trn );
+		      $display ("Random size less than MIN_VALID_LEN", trn.tr_size);
+		  end
+      
+      test_num = test_num + 1;
+
       $display ("TRANSMIT END");
     end 
   
@@ -66,28 +81,28 @@ endclass
     transaction trn;
     forever begin
       $display ("RECEIVER START");
-      wait  ( serial_val );
+      @( length_check_ev.triggered );
+      @( serial_val );
       @( posedge clk );
-      //$display ("RECEIVER BIT INPUT START");
+
       for ( int b = 0; b < DATA_WIDTH; b++ )
         if ( serial_val )
           begin 
             temp [ DATA_WIDTH - 1 - b ] = serial_out;
             @( posedge clk );
         end
-      //$display ("WAITING 4 MAIL IN");
+        
       mbx.get( trn );
-      //$display ("GOT MAIL IN");
-      if ( trn.tr_size > 2 ) 
-        for ( int t = DATA_WIDTH - 1; t < DATA_WIDTH - trn.tr_size - 1; t-- )
-          if ( temp [ t ] !== trn.data_tr [ t ] )
-            display_error ();  
 
-      test_num = test_num + 1;
+      for ( int t = DATA_WIDTH - 1; t < DATA_WIDTH - trn.tr_size - 1; t-- )
+        if ( temp [ t ] !== trn.data_tr [ t ] )
+          display_error ();  
 		  
       if ( test_num == TEST_LENGTH )  
         summary_tsk();
-		 $display ("RECEIVER FINISH", test_num );
+        
+		  $display ("RECEIVER FINISH", test_num );
+
     end
   endtask : receiver_tsk
 
@@ -156,7 +171,9 @@ module lab2_2_tb;
     mbx = new(1);
 
     fork
+      process::self.srandom(737);  
       transmit_tsk ( TEST_LENGTH, mbx );
+      process::self.srandom(373);  
       receiver_tsk ( mbx );
     join
     

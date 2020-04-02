35ed11c72f070e62c2ada88c7e141c4f00cde2ad
RKishmar <romakishmar85@gmail.com>
2020-04-02 12:47:21 +0300

    rearranged counters but tb is still a few clks ahead rtl (see state changes)

--- LAB2/lab2_4/rtl/lab2_4.sv
+++ LAB2/lab2_4/rtl/lab2_4.sv
@@ -15,17 +15,21 @@
      YELLOW_TIME_DEFAULT   dynamic   RYG_TIME [ 1 ]  Время нахождения в состоянии “желтый”  по умолчанию
      GREEN_TIME_DEFAULT    dynamic   RYG_TIME [ 2 ]  Время нахождения в состоянии “зеленый” по умолчанию
   4. Время нахождения в остальных состояниях может быть настроено динамически в ms. Для настроек исп. шину cmd_i, с кодами команд.
-
 */
 
-module lab2_4 #( parameter DATA_WIDTH          = 16,
-                 parameter CLK_I_FREQ          = 20_000_000,
-                 parameter BLINK_HALF_PERIOD   = 1000,
-                 parameter GREEN_BLINKS_NUM    = 10,
-                 parameter RED_YELLOW_TIME     = 1000,
-                 parameter RED_TIME_DEFAULT    = 1000,
-                 parameter YELLOW_TIME_DEFAULT = 1000,
-                 parameter GREEN_TIME_DEFAULT  = 1000 )
+/*
+  temp problems:
+    unknown
+*/
+
+module lab2_4 #( parameter DATA_WIDTH,           
+                 parameter CLK_I_FREQ,          
+                 parameter BLINK_HALF_PERIOD,   
+                 parameter GREEN_BLINKS_NUM,     
+                 parameter RED_YELLOW_TIME,     
+                 parameter RED_TIME_DEFAULT,     
+                 parameter YELLOW_TIME_DEFAULT,  
+                 parameter GREEN_TIME_DEFAULT )  
 ( input                          clk_i,
   input                          srst_i,
   input   [ 2 : 0 ]              cmd_type_i,
@@ -37,6 +41,7 @@ module lab2_4 #( parameter DATA_WIDTH          = 16,
   output logic                   green_o  
 );
 
+localparam                       CLK_MS_NUM   = CLK_I_FREQ / 1000_000;     // <<<<<<<<<<<<<<<<<<<
 
 localparam [ 2 : 0 ]             OFF_MODE     = 0;
 localparam [ 2 : 0 ]             STD_MODE     = 1;
@@ -62,48 +67,42 @@ assign                           green_o      = ryg_reg [ GRN ];
 
 logic      [ 2 : 0 ]             st_mode;
 
-localparam                              GREEN_BLINK_TIME            = GREEN_BLINKS_NUM * BLINK_HALF_PERIOD * 2;
-localparam logic [ DATA_WIDTH - 1 : 0 ] RYG_TIME_DEFAULTS [ 2 : 0 ] = '{ RED_TIME_DEFAULT, YELLOW_TIME_DEFAULT, GREEN_TIME_DEFAULT };
-logic            [ DATA_WIDTH - 1 : 0 ] RYG_TIME          [ 2 : 0 ] = RYG_TIME_DEFAULTS;
-
 typedef enum logic [ 2 : 0 ] { OFF_S, RED_S, RED_YEL_S, GRN_S, GRN_BLN_S, YEL_S, YEL_BLN_S } state_type;
-state_type curr_state, next_state;
+state_type curr_state = OFF_S, next_state = OFF_S;
 
 //-----------------------------------------------------------------------
 
-logic [ 31 : 0 ]  RED_CNT_MAX, R_Y_CNT_MAX, GRN_CNT_MAX, GRN_BLN_CNT_MAX, MAX_CNT;
+localparam                        GREEN_BLINK_TIME            = GREEN_BLINKS_NUM * BLINK_HALF_PERIOD * 2;
+localparam [ DATA_WIDTH - 1 : 0 ] RYG_TIME_DEFAULTS [ 2 : 0 ] = { RED_TIME_DEFAULT, YELLOW_TIME_DEFAULT, GREEN_TIME_DEFAULT };
+logic      [ DATA_WIDTH - 1 : 0 ] RYG_TIME          [ 2 : 0 ] = RYG_TIME_DEFAULTS;
+
+logic [ 31 : 0 ]  RED_CNT_MAX, R_Y_CNT_MAX, GRN_CNT_MAX, GRN_BLN_CNT_MAX, MAX_CNT, YEL_CNT_MAX;
 
 assign RED_CNT_MAX     = num_clks( RYG_TIME [ RED ] );
-assign R_Y_CNT_MAX     = num_clks( RYG_TIME [ RED ] ) + num_clks( RED_YELLOW_TIME  );
-assign GRN_CNT_MAX     = num_clks( RYG_TIME [ RED ] ) + num_clks( RED_YELLOW_TIME  ) + 
-                         num_clks( RYG_TIME [ GRN ] );
-assign GRN_BLN_CNT_MAX = num_clks( RYG_TIME [ RED ] ) + num_clks( RED_YELLOW_TIME  ) + 
-                         num_clks( RYG_TIME [ GRN ] ) + num_clks( GREEN_BLINK_TIME );
-assign MAX_CNT         = num_clks( RYG_TIME [ RED ] ) + num_clks( RED_YELLOW_TIME  ) + 
-                         num_clks( RYG_TIME [ GRN ] ) + num_clks( GREEN_BLINK_TIME ) +  
-                         num_clks( RYG_TIME [ YEL ] ); 
+assign R_Y_CNT_MAX     = num_clks( RED_YELLOW_TIME  );
+assign GRN_CNT_MAX     = num_clks( RYG_TIME [ GRN ] );
+assign GRN_BLN_CNT_MAX = num_clks( GREEN_BLINK_TIME );
+assign YEL_CNT_MAX     = num_clks( RYG_TIME [ YEL ] ); 
 
-logic [ $size( MAX_CNT ) + 1 : 0 ] light_cnt   = 0;  
-logic [ $size( MAX_CNT ) + 1 : 0 ] yel_bln_cnt = 0;             
-logic [ $size( MAX_CNT ) + 1 : 0 ] grn_bln_cnt = 0; 
+
+logic [ 31 : 0 ] light_cnt   = 0;  
+logic [ 31 : 0 ] yel_bln_cnt = 0;             
+logic [ 31 : 0 ] grn_bln_cnt = 0; 
 
 //-----------------------------------------------------------------------
 
 function int num_clks ( logic [ DATA_WIDTH - 1 : 0 ] param );
-  return ( param * ( CLK_I_FREQ / 1000 ) ); 
+  return ( param * CLK_MS_NUM ); 
 endfunction
 
 task reset_cnts ();
-
-  $display( " " );
-  $display( " DUT RESETS BLINK COUNTERS " );
-  $display( " NEXT STATE:    %b ", next_state );
-  $display( " CURRENT STATE: %b ", curr_state );
-  $display( " " );
-  
-  grn_bln_cnt <= 0;
-  yel_bln_cnt <= 0;
-  
+  begin
+    $display( " " );
+    $display( " DUT RESETS COUNTERS " );
+    grn_bln_cnt <= 0;
+    yel_bln_cnt <= 0;
+	light_cnt   <= 0;
+  end  
 endtask : reset_cnts
 
 //-----------------------------------------------------------------------
@@ -115,52 +114,41 @@ always_ff @( posedge clk_i )
     curr_state <= next_state;
  
 always_ff @( posedge clk_i )
-  begin
-    
+  begin 
     if ( !srst_i )
       begin 
-        RYG_TIME  <= RYG_TIME_DEFAULTS;
-        light_cnt <= 0;
-        st_mode   <= 0;
+        RYG_TIME <= RYG_TIME_DEFAULTS;
       end
     else 
       begin
-        light_cnt <= ( light_cnt < MAX_CNT ) ? ( light_cnt + 1 ) : 0;
         if ( cmd_valid_i == 1 )
           case ( cmd_type_i )
-            OFF_MODE, STD_MODE, YEL_BLN_MODE : 
+            OFF_MODE, YEL_BLN_MODE, STD_MODE : 
               begin 
-                light_cnt <= 0;
-                st_mode   <= cmd_type_i;
+                st_mode <= cmd_type_i;
               end
             SET_RED_DUR : RYG_TIME [ RED ] <= cmd_data_i;
             SET_GRN_DUR : RYG_TIME [ GRN ] <= cmd_data_i;
             SET_YEL_DUR : RYG_TIME [ YEL ] <= cmd_data_i;
-        endcase   
-  end
+          endcase   
+      end  
 end
  
 always_comb 
   begin
     unique case ( st_mode )
-    
       STD_MODE:
-      
-        if ( light_cnt < RED_CNT_MAX )
-          next_state = RED_S;
-        else if ( light_cnt < R_Y_CNT_MAX )
-          next_state = RED_YEL_S;
-        else if ( light_cnt < GRN_CNT_MAX )
-          next_state = GRN_S;
-        else if ( light_cnt < GRN_BLN_CNT_MAX )
-          next_state = GRN_BLN_S;
-        else 
-          next_state = YEL_S;
-		  
-      OFF_MODE     : next_state = OFF_S;
-      YEL_BLN_MODE : next_state = YEL_BLN_S;
-      default      : next_state = OFF_S;
-      
+        unique case ( curr_state )
+          RED_S     : next_state = ( light_cnt < RED_CNT_MAX     - 1 ) ? RED_S     : RED_YEL_S ;
+          RED_YEL_S : next_state = ( light_cnt < R_Y_CNT_MAX     - 1 ) ? RED_YEL_S : GRN_S     ;
+          GRN_S     : next_state = ( light_cnt < GRN_CNT_MAX     - 1 ) ? GRN_S     : GRN_BLN_S ;
+          GRN_BLN_S : next_state = ( light_cnt < GRN_BLN_CNT_MAX - 1 ) ? GRN_BLN_S : YEL_S     ;
+          YEL_S     : next_state = ( light_cnt < YEL_CNT_MAX     - 1 ) ? YEL_S     : RED_S     ;
+          default   : next_state = RED_S;
+        endcase 
+      OFF_MODE      : next_state = OFF_S;
+      YEL_BLN_MODE  : next_state = YEL_BLN_S;
+      default       : next_state = OFF_S; 
     endcase
   end
  
@@ -168,36 +156,50 @@ always_comb
 always_ff @( posedge clk_i )
   begin 
     if ( !srst_i ) 
-      ryg_reg <= off_reg;
+      begin
+        ryg_reg <= off_reg;
+      end
     else 
-      unique case ( curr_state )
-        OFF_S     : ryg_reg <= off_reg;
-        RED_S     : ryg_reg <= red_reg;
-        YEL_S     : ryg_reg <= yel_reg;
-        GRN_S     : ryg_reg <= grn_reg;
-        RED_YEL_S : ryg_reg <= r_y_reg;
-        GRN_BLN_S : 
-          begin
-            grn_bln_cnt <= ( grn_bln_cnt < 2 * BLINK_HALF_PERIOD ) ? grn_bln_cnt + 1 : 0; 
-            ryg_reg     <= ( grn_bln_cnt <     BLINK_HALF_PERIOD ) ? grn_reg : off_reg;
-          end
+      begin
+        unique case ( curr_state )
+          OFF_S     : ryg_reg <= off_reg;
+          RED_S     : ryg_reg <= red_reg;
+          YEL_S     : ryg_reg <= yel_reg;
+          GRN_S     : ryg_reg <= grn_reg;
+          RED_YEL_S : ryg_reg <= r_y_reg;
+          GRN_BLN_S : 
+            begin
+              ryg_reg     <= ( grn_bln_cnt < (     BLINK_HALF_PERIOD * CLK_MS_NUM ) ) ? grn_reg : off_reg;
+              grn_bln_cnt <= ( grn_bln_cnt < ( 2 * BLINK_HALF_PERIOD * CLK_MS_NUM ) ) ? ( grn_bln_cnt + 1 ) : 0; 
+			  $display ( " DUT grn_bln_cnt :   %0d ", grn_bln_cnt );
+            end
 
-        YEL_BLN_S : 
-          begin
-            yel_bln_cnt <= ( yel_bln_cnt < 2 * BLINK_HALF_PERIOD ) ? yel_bln_cnt + 1 : 0; 
-            ryg_reg     <= ( yel_bln_cnt <     BLINK_HALF_PERIOD ) ? yel_reg : off_reg;
-          end
+          YEL_BLN_S : 
+            begin
+              ryg_reg     <= ( yel_bln_cnt < (     BLINK_HALF_PERIOD * CLK_MS_NUM ) ) ? yel_reg : off_reg;
+              yel_bln_cnt <= ( yel_bln_cnt < ( 2 * BLINK_HALF_PERIOD * CLK_MS_NUM ) ) ? ( yel_bln_cnt + 1 ) : 0; 
+              $display( " DUT yel_bln_cnt : %0d ", yel_bln_cnt );
+            end
 	  
-        default  : 
-          begin
-            ryg_reg <= off_reg;
-          end
-      endcase	
+          default  : 
+            begin
+              ryg_reg <= off_reg;
+              $display ( " - DUT curr_state switches to default alert " );
+              $stop;
+            end
+        endcase	
     
-    if ( next_state !== curr_state )
-      reset_cnts();
-      
+        if ( next_state !== curr_state )
+          begin 
+            reset_cnts();
+          end
+		else 
+          begin
+            light_cnt <= light_cnt + 1;
+			$display( " DUT light_cnt :     %0d ", light_cnt );
+          end
+    
+      end
   end 
   
-
 endmodule 
--- LAB2/lab2_4/tb/lab2_4_tb.sv
+++ LAB2/lab2_4/tb/lab2_4_tb.sv
@@ -1,4 +1,5 @@
-`timescale 10ns/10ns
+`timescale 10ns / 10ns
+  
   
 /*
   1. последовательность: красный, красный и желтый, зелен, зеленый моргает, желтый, красный.
@@ -21,34 +22,49 @@
      Время нахождения в состоянии “красный+желтый” равно и задается статически параметром в ms.
      Время нахождения в остальных состояниях может быть настроено динамически в ms. Для настроек исп. шину cmd_i, с кодами команд.
 
+     
 */
 
 
 
 module lab2_4_tb;
 
-localparam CLK_I_FREQ_T = 20_000_000;
-localparam CLK_MS_NUM_T = CLK_I_FREQ_T / 1000;
-localparam DATA_WIDTH_T = 16;
+localparam           CLK_HLF_PER  = 2;
+localparam           CLK_I_FREQ_T = 20_000_000;
+localparam           CLK_MS_NUM_T = CLK_I_FREQ_T / 1000_000;
+localparam           DATA_WIDTH_T = 16;
 
-localparam MIN_DATA_RND = 50;  
-localparam MAX_DATA_RND = 500; 
-localparam TEST_LENGTH  = 300;
+localparam           TEST_LENGTH  = 3000;
 
-localparam BLNK_HLF_PER = 400;
-localparam GRN_BLNK_NUM = 5;
-localparam RED_YLW_TIME = 300;
-localparam RED_TIME_DFT = 700;
-localparam YLW_TIME_DFT = 300;
-localparam GRN_TIME_DFT = 700;
+localparam           TIME_QUANT   = 20;
+localparam           GRN_BLNK_NUM = 6;
+localparam           RED_YLW_TIME = 3 * TIME_QUANT;
+localparam           RED_TIME_DFT = 5 * TIME_QUANT;
+localparam           YLW_TIME_DFT = 3 * TIME_QUANT;
+localparam           GRN_TIME_DFT = 5 * TIME_QUANT;
+localparam           MIN_DATA_RND = 3 * TIME_QUANT;  
+localparam           MAX_DATA_RND = 5 * TIME_QUANT; 
+localparam           BLNK_HLF_PER = 1 * TIME_QUANT;
+localparam           BLINK_PERIOD    = 2 * BLNK_HLF_PER * CLK_MS_NUM_T; 
+localparam           GREEN_BLNK_TIME = 2 * BLNK_HLF_PER * GRN_BLNK_NUM;
 
-localparam [ 2 : 0 ] red_reg = 3'b100;
-localparam [ 2 : 0 ] yel_reg = 3'b010;
-localparam [ 2 : 0 ] grn_reg = 3'b001;
-localparam [ 2 : 0 ] r_y_reg = 3'b110;
-localparam [ 2 : 0 ] off_reg = 3'b000;
+localparam [ 2 : 0 ] OFF_MODE     = 0;
+localparam [ 2 : 0 ] STD_MODE     = 1;
+localparam [ 2 : 0 ] YEL_BLN_MODE = 2;
+localparam [ 2 : 0 ] SET_GRN_DUR  = 3;
+localparam [ 2 : 0 ] SET_RED_DUR  = 4;
+localparam [ 2 : 0 ] SET_YEL_DUR  = 5;
 
-localparam                          GREEN_BLNK_TIME             = GRN_BLNK_NUM * BLNK_HLF_PER * 2;
+localparam [ 2 : 0 ] RED          = 0;
+localparam [ 2 : 0 ] YEL          = 1;
+localparam [ 2 : 0 ] GRN          = 2;
+
+localparam [ 2 : 0 ] red_reg      = 3'b100;
+localparam [ 2 : 0 ] yel_reg      = 3'b010;
+localparam [ 2 : 0 ] grn_reg      = 3'b001;
+localparam [ 2 : 0 ] r_y_reg      = 3'b110;
+localparam [ 2 : 0 ] off_reg      = 3'b000;
+
 localparam [ DATA_WIDTH_T - 1 : 0 ] RYG_TIME_DEFAULTS [ 2 : 0 ] = { RED_TIME_DFT, YLW_TIME_DFT, GRN_TIME_DFT };
 logic      [ DATA_WIDTH_T - 1 : 0 ] RYG_TIME          [ 2 : 0 ] = RYG_TIME_DEFAULTS;
 
@@ -59,28 +75,47 @@ logic                          cmd_valid_t;
 logic [ DATA_WIDTH_T - 1 : 0 ] cmd_data_t;
 
 logic [ 2 : 0 ]                ryg_reg  ;
+logic [ 2 : 0 ]                grn_bln  ;
+logic [ 2 : 0 ]                yel_bln  ;
 logic [ 2 : 0 ]                ryg_reg_t;
 
-int                            cnt = 0;
-int                            errors  = 0, test_num   = 0;
-bit                            rcv_end = 0, trm_end    = 0;
+int                            cnt;
+int                            errors, test_num;
+bit                            rcv_end, trm_end;
 
-static int             cnt_t [ 2 : 0 ] = { 0, 0, 0 }; 
-static logic [ 2 : 0 ] state_t = 0;
-  
+static logic [ DATA_WIDTH_T + 15 : 0 ] std_cnt, ybl_cnt, gbl_cnt; 
+static logic [ 2 : 0 ]                 mode_t, curr_mode;
+
+//-----------------------------------------------------------------------
+
+logic [ 31 : 0 ]  RED_CNT_MAX, R_Y_CNT_MAX, GRN_CNT_MAX, GRN_BLN_CNT_MAX, MAX_CNT;
+
+function int num_clks ( logic [ DATA_WIDTH_T - 1 : 0 ] param );
+  return ( param * CLK_MS_NUM_T ); 
+endfunction
+
+assign     RED_CNT_MAX     = num_clks ( RYG_TIME [ RED ] );
+assign     R_Y_CNT_MAX     = num_clks ( RYG_TIME [ RED ] ) + num_clks( RED_YLW_TIME );
+assign     GRN_CNT_MAX     = num_clks ( RYG_TIME [ RED ] ) + num_clks( RED_YLW_TIME ) + num_clks( RYG_TIME [ GRN ] );
+assign     GRN_BLN_CNT_MAX = num_clks ( RYG_TIME [ RED ] ) + num_clks( RED_YLW_TIME ) + num_clks( RYG_TIME [ GRN ] ) + num_clks( GREEN_BLNK_TIME );
+assign     MAX_CNT         = num_clks ( RYG_TIME [ RED ] ) + num_clks( RED_YLW_TIME ) + num_clks( RYG_TIME [ GRN ] ) + num_clks( GREEN_BLNK_TIME ) + num_clks( RYG_TIME [ YEL ] ); 
+localparam MIN_DURN_RND    = num_clks ( 24 * TIME_QUANT  );  
+localparam MAX_DURN_RND    = num_clks ( 32 * TIME_QUANT  );  
+ 
 //--------------------------------------------------------------------------------
 
 class transaction;
-  rand bit [ DATA_WIDTH_T - 1 : 0 ] cmd_data_tr;
-  rand bit                          cmd_valid_tr;
-  rand bit [ 2 : 0 ]                cmd_type_tr;
-  rand int                          duration_tr;
+  rand bit [ DATA_WIDTH_T + 15 : 0 ] cmd_data_tr;
+  rand bit                           cmd_valid_tr;
+  rand bit [ 2 : 0 ]                 cmd_type_tr;
+  rand int                           test_duration_tr;
   
   function void randomizes ();
-    cmd_data_tr  = $urandom_range( MIN_DATA_RND, MAX_DATA_RND );
+    process::self.srandom( $realtime ); 
+    cmd_data_tr  = $urandom_range( MAX_DATA_RND, MIN_DATA_RND );
     cmd_valid_tr = $urandom_range( 0, 1 );
     cmd_type_tr  = $urandom_range( 0, 5 );	
-    duration_tr  = $urandom_range( MAX_DATA_RND, 2 * MAX_DATA_RND );
+    test_duration_tr  = $urandom_range( MAX_DURN_RND, MIN_DURN_RND );
   endfunction
   
 endclass
@@ -88,26 +123,27 @@ endclass
 //---------------------------------------------------------------------------- 
  
 task transmit_tsk( input int unsigned n,
-                   input mailbox #( transaction ) mbx );
-    
+                   input mailbox #( transaction ) mbx );  
   automatic transaction trn = new();
     
-  repeat ( n ) begin   
-       
+  repeat ( n ) begin  
+  
+    trm_end = 0;  
+    wait ( rcv_end ); 
+
+    $display ( " TB TRANSMIT START " );  
+	
     trn.randomizes();
     mbx.put( trn );
-	
-	trm_end = 1;
-	#1;
-    trm_end = 0;
-	 
+   
     cmd_data_t  = trn.cmd_data_tr;
     cmd_type_t  = trn.cmd_type_tr;
     cmd_valid_t = trn.cmd_valid_tr;
 
     test_num = test_num + 1;
-	
-    wait ( rcv_end );
+     
+    trm_end = 1;
+    #1; //wait ( !rcv_end );
 	
   end
 
@@ -119,152 +155,122 @@ task receiver_tsk( input mailbox #( transaction ) mbx );
   transaction trn;
   
   forever begin
-
+    $display( " TB cnt :            %0d", cnt );
     if ( cnt == 0 ) 
-      begin
-        
-      rcv_end = 1;
-      wait ( trm_end );
-      rcv_end = 0;
-		
-        mbx.get( trn );
-		
-        if ( trn.cmd_valid_tr == 1 )
-          begin 
-            unique case ( trn.cmd_type_tr ) 
-              0:
-                begin
-                  $display( " " );
-                  $display( " ~  STATE CHANGE TO: OFF STATE " );
-                  state_t = 0;
-                  cnt_t   = { 0, 0, 0 };
-              end
-			  
-              1:
-                begin
-                  $display( " " );
-                  $display( " ~  STATE CHANGE TO: STANDARD " );
-                  state_t = 1;
-                  cnt_t   = { 0, 0, 0 };
-              end	
-			  
-              2:
-                begin
-                  $display( " " );
-                  $display( " ~  STATE CHANGE TO: UNREGULATED " );
-                  state_t = 2;
-                  cnt_t   = { 0, 0, 0 };
-              end
-			  
-              3:
-                begin 
-                  RYG_TIME [ 2 ] = trn.cmd_data_tr;
-              end
-			  
-              4:
-                begin 
-                  RYG_TIME [ 0 ] = trn.cmd_data_tr;                 
-              end
-			  
-              5:
-                begin 
-                  RYG_TIME [ 1 ] = trn.cmd_data_tr;                
-              end
-			  
-            endcase
-        end
-
-        cnt = RYG_TIME [ state_t ];   
-  
-    end else 
       begin 
-		
-        unique case ( state_t ) 
-          0:
-            begin
-              ryg_reg_t = off_reg;
-          end
-	  
-          1:
-            begin 
-              cnt_t [ 1 ] = cnt_t [ 1 ] + 1;
-	            if ( cnt_t [ 1 ] < ( CLK_MS_NUM_T * RYG_TIME [ 0 ] ) ) 
-                  begin
-                    ryg_reg_t = red_reg;
-                end else if ( cnt_t [ 1 ] < ( CLK_MS_NUM_T * RYG_TIME [ 0 ] + CLK_MS_NUM_T * RED_YLW_TIME ) )
-                  begin
-                    ryg_reg_t = r_y_reg;
-                end else if ( cnt_t [ 1 ] < ( CLK_MS_NUM_T * RYG_TIME [ 0 ] + CLK_MS_NUM_T * RED_YLW_TIME + 
-                                              CLK_MS_NUM_T * RYG_TIME [ 2 ] ) )
-                  begin
-                    ryg_reg_t = grn_reg;
-                end else if ( cnt_t [ 1 ] < ( CLK_MS_NUM_T * RYG_TIME [ 0 ] + CLK_MS_NUM_T * RED_YLW_TIME + 
-                                              CLK_MS_NUM_T * RYG_TIME [ 2 ] + CLK_MS_NUM_T * GREEN_BLNK_TIME ) )
-                  begin
-                    cnt_t [ 0 ] = cnt_t [ 0 ] + 1; 
-                    if ( cnt_t [ 0 ] < BLNK_HLF_PER * CLK_MS_NUM_T ) 
-                      begin
-                        ryg_reg_t = grn_reg;
-                    end else if ( cnt_t [ 0 ] < ( 2 * BLNK_HLF_PER * CLK_MS_NUM_T ) ) 
-                      begin
-                        ryg_reg_t = off_reg;
-                    end else 
-                      cnt_t [ 0 ] = 0;
-                   
-                end else if ( cnt_t [ 1 ] < ( CLK_MS_NUM_T * RYG_TIME [ 0 ] + CLK_MS_NUM_T * RED_YLW_TIME + 
-                                              CLK_MS_NUM_T * RYG_TIME [ 2 ] + CLK_MS_NUM_T * GREEN_BLNK_TIME +  
-                                              CLK_MS_NUM_T * RYG_TIME [ 1 ] ) )
-                  begin
-                    ryg_reg_t = yel_reg;
-                end else 
-                  cnt_t [ 1 ] = 0;
-          end
-
-          2:
-            begin
-              cnt_t [ 2 ] = cnt_t [ 2 ] + 1; 
-              if ( cnt_t [ 2 ] < BLNK_HLF_PER * CLK_MS_NUM_T ) 
-                begin
-                  ryg_reg_t = yel_reg;
-              end else if ( cnt_t [ 2 ] < ( 2 * BLNK_HLF_PER * CLK_MS_NUM_T ) ) 
-                begin
-                  ryg_reg_t = off_reg;
-              end else 
-                cnt_t [ 2 ] = 0;
-          end	 
-           
-        endcase
+        rcv_end = 1;
+        wait ( trm_end );
+        rcv_end = 0;	  
         
-		//----------------------------------------------------------------------------------
-		
-    end
-      
-  cnt = cnt - 1;
+        mbx.get( trn );
+        change_mode_if_valid ( trn );
+        		
+      end 
+ 
+    set_lights   ( );
+    set_counters ( trn );
 	
-  @( posedge clk_t );
-
     if ( ryg_reg !== ryg_reg_t ) 
       report_error();
     else
       report_success();
 
-  if ( test_num == TEST_LENGTH )  
-    summary_tsk();
-  
+    if ( test_num == TEST_LENGTH )  
+      summary_tsk();
+	  
   end
 endtask : receiver_tsk
 
-//---------------------------------------------------------------------------- 
+
+task automatic set_lights ( );
+  begin
+    grn_bln = ( gbl_cnt <= BLINK_PERIOD / 2 ) ?  grn_reg : off_reg;
+    yel_bln = ( ybl_cnt <= BLINK_PERIOD / 2 ) ?  yel_reg : off_reg;   
+    unique case ( mode_t ) 
+      OFF_MODE:
+        begin $display( " TB MODE : OFF_MODE " );
+          ryg_reg_t = off_reg; 
+        end
+      STD_MODE:
+        begin $display( " TB MODE : STD_MODE, %0d / %0d / %0d / %0d / %0d  ", std_cnt, RED_CNT_MAX, R_Y_CNT_MAX, GRN_CNT_MAX, GRN_BLN_CNT_MAX );
+          if      ( std_cnt <= RED_CNT_MAX       ) begin ryg_reg_t = red_reg; end
+          else if ( std_cnt <= R_Y_CNT_MAX       ) begin ryg_reg_t = r_y_reg; end
+          else if ( std_cnt <= GRN_CNT_MAX       ) begin ryg_reg_t = grn_reg; end
+          else if ( std_cnt <= GRN_BLN_CNT_MAX   ) begin ryg_reg_t = grn_bln; end
+          else                                     begin ryg_reg_t = yel_reg; end
+        end
+      YEL_BLN_MODE:
+        begin $display( " TB MODE : YEL_BLN_MODE, %0d ", ybl_cnt );
+          ryg_reg_t = yel_bln; 	  
+        end	 
+    endcase
+	
+  end
+endtask : set_lights
+
+//=========================================================================================================
+
+task automatic set_counters ( transaction sc_trn );
+  begin
+    if ( mode_t == STD_MODE ) 
+      begin
+        std_cnt = ( std_cnt <= MAX_CNT ) ? ( std_cnt + 1 ) : 0;
+        if ( ( std_cnt <= GRN_BLN_CNT_MAX ) & ( std_cnt >= GRN_CNT_MAX ) ) 
+          gbl_cnt = ( gbl_cnt <= BLINK_PERIOD ) ? ( gbl_cnt + 1 ) : 0;
+      end
+    else if ( mode_t == YEL_BLN_MODE )
+      begin
+        ybl_cnt   = ( ybl_cnt <= BLINK_PERIOD ) ? ( ybl_cnt + 1 ) : 0;   
+      end	 
+	
+    if ( cnt == 0 )
+      begin
+        cnt = sc_trn.test_duration_tr; 
+        @( posedge clk_t ); @( posedge clk_t ); @( posedge clk_t );
+      end
+    else 
+      begin
+        cnt = cnt - 1;
+        @( posedge clk_t );
+      end	
+  end
+endtask : set_counters
+
+//========================================================================================================
+
+task automatic change_mode_if_valid ( transaction chm_trn );
+  automatic bit                          val  = chm_trn.cmd_valid_tr; 
+  automatic bit [ 2 : 0 ]                comm = chm_trn.cmd_type_tr; 
+  automatic bit [ DATA_WIDTH_T - 1 : 0 ] data = chm_trn.cmd_data_tr;
+    begin  
+      if ( val == 1 )
+        begin 
+          curr_mode = mode_t;		
+          unique case ( comm ) 
+            OFF_MODE    : mode_t = OFF_MODE;
+            STD_MODE    : mode_t = STD_MODE;	
+            YEL_BLN_MODE: mode_t = YEL_BLN_MODE;
+			
+            SET_GRN_DUR : RYG_TIME [ GRN ] = data;
+            SET_RED_DUR : RYG_TIME [ RED ] = data;                 
+            SET_YEL_DUR : RYG_TIME [ YEL ] = data;                  
+          endcase
+        end	
+  end
+endtask : change_mode_if_valid
+
+
+//======================================================================================
+
 
 task automatic report_error ();
   begin
     errors = errors + 1;
     $display( " " );
-    $display( " -  Error test  #                 %d ", test_num                      );
-    $display( "    traffic light state expected: %b ", ryg_reg_t                     );
-    $display( "    traffic light state actual:   %b ", ryg_reg                       ); 
-    $display( "    State expected:               %d ", state_t                       );
-    $display( "    count length / cnt: %d /      %d ", RYG_TIME [ state_t ], cnt );	  
+    $display( " -  Error test  # %0d ",                   test_num  );
+    $display( "    traffic light ryg_reg expected : %b ", ryg_reg_t );
+    $display( "    traffic light ryg_reg actual   : %b ", ryg_reg   ); 
+	  
     $stop;
   end
 endtask : report_error
@@ -272,14 +278,14 @@ endtask : report_error
 task automatic report_success ();
   begin
     $display( " " );
-    $display( " +  TEST SUCCESS, trafic light state: %b ", ryg_reg );  
+    $display( " +  TEST SUCCESS, trafic light ryg_reg: %b ", ryg_reg );  
   end 
 endtask : report_success
 
 task automatic summary_tsk ();
   begin
     $display( " " );
-    $display( "  Summary: %d tests completed with %d error(s) ", test_num, errors );
+    $display( "  Summary: %0d tests completed with %0d error(s) ", test_num, errors );
     $display( " " );
     $stop;    
   end  
@@ -292,51 +298,54 @@ mailbox #( transaction ) mbx;
 //----------------------------------------------------------------------------  
   
 lab2_4 #( 
-  .DATA_WIDTH          ( DATA_WIDTH_T ),
-  .CLK_I_FREQ          ( CLK_I_FREQ_T ),
-  .BLINK_HALF_PERIOD   ( BLNK_HLF_PER ),
-  .GREEN_BLINKS_NUM    ( GRN_BLNK_NUM ),
-  .RED_YELLOW_TIME     ( RED_YLW_TIME ),
-  .RED_TIME_DEFAULT    ( RED_TIME_DFT ),
-  .YELLOW_TIME_DEFAULT ( YLW_TIME_DFT ),
-  .GREEN_TIME_DEFAULT  ( GRN_TIME_DFT ))
+  .DATA_WIDTH          ( DATA_WIDTH_T   ),
+  .CLK_I_FREQ          ( CLK_I_FREQ_T   ),
+  .BLINK_HALF_PERIOD   ( BLNK_HLF_PER   ),
+  .GREEN_BLINKS_NUM    ( GRN_BLNK_NUM   ),
+  .RED_YELLOW_TIME     ( RED_YLW_TIME   ),
+  .RED_TIME_DEFAULT    ( RED_TIME_DFT   ),
+  .YELLOW_TIME_DEFAULT ( YLW_TIME_DFT   ),
+  .GREEN_TIME_DEFAULT  ( GRN_TIME_DFT   ))
 DUT
-( .clk_i               ( clk_t        ),
-  .srst_i              ( srst_t       ),
-  .cmd_type_i          ( cmd_type_t   ),
-  .cmd_valid_i         ( cmd_valid_t  ),  
-  .cmd_data_i          ( cmd_data_t   ),
-  .red_o               ( ryg_reg [0]  ),
-  .yellow_o            ( ryg_reg [1]  ),
-  .green_o             ( ryg_reg [2]  )  
+( .clk_i               ( clk_t          ),
+  .srst_i              ( srst_t         ),
+  .cmd_type_i          ( cmd_type_t     ),
+  .cmd_valid_i         ( cmd_valid_t    ),  
+  .cmd_data_i          ( cmd_data_t     ),
+  .red_o               ( ryg_reg [RED]  ),
+  .yellow_o            ( ryg_reg [YEL]  ),
+  .green_o             ( ryg_reg [GRN]  )  
 );
 
 //---------------------------------------------------------------------------- 
   
   always begin
-    clk_t = 1; #5; 
-    clk_t = 0; #5;
+    clk_t = 1; #CLK_HLF_PER; 
+    clk_t = 0; #CLK_HLF_PER;
   end
 
   initial begin
     ryg_reg_t = off_reg;
-    srst_t    = 1;
+    cnt       = 0; 
+    std_cnt   = 0; 
+    ybl_cnt   = 0; 
+    gbl_cnt   = 0; 
+    mode_t    = 0;
+    curr_mode = mode_t;
     errors    = 0;
     test_num  = 0;
-    @( posedge clk_t );
-    srst_t = 0;
-    @( posedge clk_t );
-    srst_t = 1;
+    rcv_end   = 0; 
+    trm_end   = 0;
+    
+    srst_t    = 1;
     
   end
 
   initial begin
     mbx = new( 1 );
 
-    fork
-      process::self.srandom( 373 );  
+    fork 
       transmit_tsk ( TEST_LENGTH, mbx );
-      process::self.srandom( 737 );  
       receiver_tsk ( mbx );
     join
     
